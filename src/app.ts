cfa81959298dc1bd584343ea7925b3ad668e037c
amalrajsr <sramalraj123@gmail.com>
2026-07-03 17:31:09 +0530

    fix: update CORS origin to use environment variable

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,14 @@ import express from "express";
 import cors from "cors";
 import { errorHandler, notFoundHandler } from "./middleware/error.middleware";
 import expenseRoutes from "./modules/expenses/expense.routes";
+import "dotenv/config";
 
 const app = express();
 app.use(express.json());
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: process.env.CORS_ORIGIN,
     methods: ["GET", "POST", "DELETE"],
   }),
 );
